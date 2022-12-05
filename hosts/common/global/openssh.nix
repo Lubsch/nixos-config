81d2e63a1c4dedbee7b955287dfebc7071854721
Lubsch <automaticcraft04@gmail.com>
2022-12-05 02:16:55 +0100

    Changed path typo

--- hosts/common/global/openssh.nix
+++ hosts/common/global/openssh.nix
@@ -18,7 +18,7 @@ in
     gatewayPorts = "clientspecified";
 
     hostKeys = [{
-      path = "$/persist/etc/ssh/ssh_host_ed25519_key";
+      path = "/persist/etc/ssh/ssh_host_ed25519_key";
       type = "ed25519";
     }];
   };
