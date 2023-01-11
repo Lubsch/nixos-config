11159554cf77b48e7eaa92b8a6abd864e059998c
blohmar <b.lohmar@campus.tu-berlin.de>
2023-01-11 15:13:49 +0000

    added duke system requirements

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,11 @@
 let
   archUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIS5t/83PuKQJx7NjcWqv4nJsQLpT7GAN9gWDdGnzP9e";
   users = [ archUser ];
+
+  dukeHost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINxylSkwlorFuPTKb+yEAR7DgixasqA8UqcgoLx8h1vx";
+  hosts = [ dukeHost ];
 in
 {
-  "userPassword.age".publicKeys = users;
-  "wireless.age".publicKeys = users;
+  "userPassword.age".publicKeys = users ++ hosts;
+  "wireless.age".publicKeys = users ++ hosts;
 }
