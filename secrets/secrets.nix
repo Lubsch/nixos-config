8e32f76f9f1f76e1af28234cce61ce0ed2de6bb1
blohmar <b.lohmar@campus.tu-berlin.de>
2023-01-11 15:32:26 +0000

    added encrypted wifi password and phone key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   archUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIS5t/83PuKQJx7NjcWqv4nJsQLpT7GAN9gWDdGnzP9e";
-  users = [ archUser ];
+  earlUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMvuIIrh2iuj2hX0zIzqLUC/5SD/ZJ3GaLcI1AyHDQuM";
+  users = [ archUser earlUser ];
 
   dukeHost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINxylSkwlorFuPTKb+yEAR7DgixasqA8UqcgoLx8h1vx";
   hosts = [ dukeHost ];
