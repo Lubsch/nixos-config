903c4650f76a2b408a9ea005efc8d9e094498bd3
blohmar <b.lohmar@campus.tu-berlin.de>
2023-01-11 14:50:47 +0000

    final setup.sh hopefully

--- setup.sh
+++ setup.sh
@@ -12,19 +12,21 @@ fi
 # Partition the drive
 parted $2 mklabel gpt
 parted $2 mkpart ESP fat32 1MiB 513MiB
-parted $2 set ESP esp on
-parted $2 mkpart $1_crypt btrfs 513MB 100%
+parted $2 set 1 esp on
+parted $2 mkpart "$1_crypt" btrfs 513MiB 100%
+
+sleep 0.5
 
 # Create ESP file system
 mkfs.vfat /dev/disk/by-partlabel/ESP
 
 # Setup encryption and temporarily unencrypt
-cryptsetup --verify-passphrase -v luksFormat /dev/disk/by-partlabel/$1_crypt
-cryptsetup open /dev/disk/by-partlabel/$1_crypt enc
+cryptsetup --verify-passphrase -v luksFormat /dev/disk/by-partlabel/"$1_crypt"
+cryptsetup open /dev/disk/by-partlabel/"$1_crypt" enc
 mkfs.btrfs /dev/mapper/enc
 
 # Create subvolumes and snapshot empty root subvolume
-mkdir /btrfs
+mkdir -p /btrfs
 mount -t btrfs /dev/mapper/enc /btrfs
 btrfs subvolume create /btrfs/root
 btrfs subvolume create /btrfs/nix
@@ -42,11 +44,11 @@ mount -o subvol=root,compress=zstd,noatime /dev/mapper/enc /mnt/persist
 mkdir /mnt/swap
 mount -o subvol=root,noatime /dev/mapper/enc /mnt/swap
 mkdir /mnt/boot
-mount /dev/disk/by-label/ESP /mnt/boot
+mount /dev/disk/by-partlabel/ESP /mnt/boot
 
 # Generate ssh-hostkeys
 mkdir -p /mnt/etc/ssh
 ssh-keygen -A -f /mnt -C "root@$1"
 
 # Output host keys and auto generated config to magic-wormhole
-printf "%s\n%s" "$(nixos-generate-config --root /mnt --show-hardware-config)" "$(cat /mnt/etc/ssh/ssh_host_ed25519_key.pub)" | wormhole send
+echo -e "$(nixos-generate-config --root /mnt --show-hardware-config)" "\n" "$(cat /mnt/etc/ssh/ssh_host_ed25519_key.pub)" | wormhole send --text -
