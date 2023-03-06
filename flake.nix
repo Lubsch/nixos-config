4ce1a64e7d64ad2b3524588a5e8908d68e0a0c14
Lubsch <automaticcraft04@gmail.com>
2023-03-06 16:00:09 +0100

    Removed duke.nix

--- flake.nix
+++ flake.nix
@@ -22,52 +22,49 @@
     };
   };
 
-  outputs = { self, nixpkgs, hardware, impermanence, home-manager, firefox-addons, nix-colors, ... }@inputs:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in {
-      templates = import ./templates;
-      overlays = import ./overlays;
+  outputs = { self, nixpkgs, hardware, impermanence, home-manager, firefox-addons, nix-colors, ... }: {
+    templates = import ./templates;
+    overlays = import ./overlays;
 
-      nixosConfigurations = {
-        "duke" = nixpkgs.lib.nixosSystem {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          modules = [
-            { config._module.args = {
-                hostname = "duke";
-                system = "x86_64-linux";
-                kernelModules = [ "kvm-intel" ];
-                initrdModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
-                users.lubsch.arguments.authorizedKeys = [
-                  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+woFGMkb7kaOxHCY8hr6/d0Q/HIHIS3so7BANQqUe6" # arch
-                  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMvuIIrh2iuj2hX0zIzqLUC/5SD/ZJ3GaLcI1AyHDQuM" # droid
-                ];
-            }; }
-            impermanence.nixosModules.impermanence
-            ./nixos/common
-            ./nixos/wireless.nix
-            ./nixos/virtualisation.nix
-            ./nixos/pipewire.nix
-          ];
-        };
-      };
-
-      homeConfigurations = {
-        "lubsch@duke" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          modules = [
-            { config._module.args = {
-                username = "lubsch";
-                hostname = "duke";
-                inherit (nix-colors) colorSchemes;
-                firefox-addons = firefox-addons.packages.x86_64-linux;
-            }; }
-            impermanence.nixosModules.home-manager.impermanence
-            ./home/common
-            ./home/nvim
-          ];
-        };
+    nixosConfigurations = {
+      "duke" = nixpkgs.lib.nixosSystem {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          { config._module.args = {
+              hostname = "duke";
+              system = "x86_64-linux";
+              kernelModules = [ "kvm-intel" ];
+              initrdModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
+              cpuFreqGovernor = "powersave";
+              users.lubsch.arguments.authorizedKeys = [
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+woFGMkb7kaOxHCY8hr6/d0Q/HIHIS3so7BANQqUe6" # arch
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMvuIIrh2iuj2hX0zIzqLUC/5SD/ZJ3GaLcI1AyHDQuM" # droid
+              ];
+          }; }
+          impermanence.nixosModules.impermanence
+          ./nixos/common
+          ./nixos/wireless.nix
+          ./nixos/virtualisation.nix
+          ./nixos/pipewire.nix
+        ];
       };
     };
+
+    homeConfigurations = {
+      "lubsch@duke" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          { config._module.args = {
+              username = "lubsch";
+              hostname = "duke";
+              inherit (nix-colors) colorSchemes;
+              firefox-addons = firefox-addons.packages.x86_64-linux;
+          }; }
+          impermanence.nixosModules.home-manager.impermanence
+          ./home/common
+          ./home/nvim
+        ];
+      };
+    };
+  };
 }
