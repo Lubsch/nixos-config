3a219785dc87393f0571c5c0dcda90938b87c1de
Lubsch <automaticcraft04@gmail.com>
2023-03-10 15:36:08 +0100

    foot colorscheme

--- home/desktop/common/foot.nix
+++ home/desktop/common/foot.nix
@@ -7,6 +7,27 @@
         font = "${fonts.mono.name}:size=11";
         dpi-aware = "yes";
       };
+      colors = {
+          regular0 = colors.base00;
+          regular1 = colors.base01;
+          regular2 = colors.base02;
+          regular3 = colors.base03;
+          regular4 = colors.base04;
+          regular5 = colors.base05;
+          regular6 = colors.base06;
+          regular7 = colors.base07;
+          bright0 = colors.base08;
+          bright1 = colors.base09;
+          bright2 = colors.base0A;
+          bright3 = colors.base0B;
+          bright4 = colors.base0C;
+          bright5 = colors.base0D;
+          bright6 = colors.base0E;
+          bright7 = colors.base0F;
+
+          foreground = colors.base05;
+          background = colors.base00;
+      };
     };
   };
 }
