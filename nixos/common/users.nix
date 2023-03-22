a9c725c08f6d361f02625af4de88fe28bc8e378e
blohmar <b.lohmar@campus.tu-berlin.de>
2023-03-22 02:26:52 +0000

    removed from users.nix

--- nixos/common/users.nix
+++ nixos/common/users.nix
@@ -1,34 +1,22 @@
 { config, pkgs, inputs, impermanence, ... }@args:
 if (args ? users) then {
-  imports = [ inputs.home-manager.nixosModules.home-manager ];
-    users = {
-      mutableUsers = false;
+  users = {
+    mutableUsers = false;
 
-      users = builtins.mapAttrs
-        (username: user: {
-          isNormalUser = true;
-          shell = pkgs.zsh;
-          extraGroups = [
-            "wheel"
-            "networkmanager"
-            "libvirtd"
-          ];
-          openssh.authorizedKeys = { inherit (config) keys; };
-          # TODO Make this work without /persist existing, too
-          passwordFile = "/persist/passwords/${username}";
-        })
-        args.users;
-    };
-    home-manager = {
-      extraSpecialArgs = { inherit inputs impermanence; };
-      useGlobalPkgs = true;
-      useUserPackages = true;
-
-      users = builtins.mapAttrs 
-        (username: user: {
-          inherit (user) imports;
-          _module.args = { inherit username; }; })
-        args.users;
+    users = builtins.mapAttrs
+      (username: user: {
+        isNormalUser = true;
+        shell = pkgs.zsh;
+        extraGroups = [
+          "wheel"
+          "networkmanager"
+          "libvirtd"
+        ];
+        openssh.authorizedKeys = { inherit (config) keys; };
+        # TODO Make this work without /persist existing, too
+        passwordFile = "/persist/passwords/${username}";
+      })
+      args.users;
   };
 } else {
   users.users.root.openssh.authorizedKeys = { inherit (config) keys; };
