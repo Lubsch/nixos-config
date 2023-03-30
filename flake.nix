bd17bf9f27f9518f4aeba82ba887129b55f0e865
Lubsch <33580245+Lubsch@users.noreply.github.com>
2023-03-30 17:04:40 +0200

    fix .. to .

--- flake.nix
+++ flake.nix
@@ -17,11 +17,11 @@
 
   outputs = { nixpkgs, ... }@inputs: {
     templates = import ./templates;
-    packages = nixpkgs.lib.genAttrs 
-      [ "x86_64-linux" "aarch64-linux" ]
-      (system: { 
-       nvim = import ../home/nvim/package.nix nixpkgs.legacyPackages.${system}; 
-       });
+
+    packages = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ]
+      (system: let pkgs = nixpkgs.legacyPackages.${system}; in {
+        nvim = import ./home/nvim/package.nix pkgs;
+      });
 
     nixosConfigurations = {
       "duke" = nixpkgs.lib.nixosSystem {
