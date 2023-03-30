908d5f2f71f1749e2441fcf597518d39d6ecc7c6
Lubsch <33580245+Lubsch@users.noreply.github.com>
2023-03-30 14:48:24 +0200

    restore good flake.nix

--- flake.nix
+++ flake.nix
@@ -15,9 +15,11 @@
     };
   };
 
-  outputs = { home-manager, nixpkgs, ... }@inputs: 
-  let
-    hosts = {
+  outputs = { home-manager, nixpkgs, ... }@inputs: {
+    templates = import ./templates;
+    packages = import ./pkgs nixpkgs;
+
+    nixosConfigurations = {
       "duke" = {
         modules = [
           ./nixos/common
@@ -26,6 +28,7 @@
           ./nixos/zsh.nix
         ];
         specialArgs = {
+          inherit inputs;
           system = "x86_64-linux";
           impermanence = true;
           # doas btrfs inspect-internal map-swapfile -r /swap/swapfile
@@ -35,41 +38,15 @@
           initrdModules= [ 
             "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_usb_sdmmc"
           ];
-          users = [ "lubsch" ];
+          users."lubsch" = [
+            ./home/common
+            ./home/nvim.nix
+            ./home/desktop-common
+            ./home/hyprland.nix
+            ./home/dwl.nix
+          ];
         };
       };
     };
-    users = {
-      "lubsch" = {
-      	hostname = "duke";
-        modules = [
-          ./home/common
-          ./home/nvim.nix
-          ./home/desktop-common
-          ./home/hyprland.nix
-          ./home/dwl.nix
-        ];
-      };
-    };
-    in {
-      templates = import ./templates;
-      packages = import ./pkgs nixpkgs;
-
-      homeConfigurations = nixpkgs.lib.mapAttrs'
-        (username: user: with user; {
-          name = "${username}@${hostname}";
-            value = let host = hosts.${hostname}; in
-            home-manager.lib.homeManagerConfiguration {
-              inherit modules;
-              pkgs = nixpkgs.legacyPackages.${host.specialArgs.system};
-              extraSpecialArgs = { inherit username inputs host; };
-          };
-        })
-        users;
-
-      nixosConfigurations = builtins.mapAttrs
-        (hostname: host: nixpkgs.lib.nixosSystem 
-          (host // { specialArgs = { inherit hostname inputs; }; }))
-        hosts;
-    };
+  };
 }
