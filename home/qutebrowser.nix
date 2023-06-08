2850f51d01689e05ddb88ce4d797f1715d2a53de
Lubsch <33580245+Lubsch@users.noreply.github.com>
2023-06-08 14:18:17 +0200

    make qutebrowser default

--- home/qutebrowser.nix
+++ home/qutebrowser.nix
@@ -1,7 +1,20 @@
-{
+{ lib, pkgs, ... }: {
+  home.sessionVariables.BROWSER = lib.mkForce "qutebrowser";
+
   programs.qutebrowser = {
     enable = true;
     extraConfig = ''
+      config.unbind("<Ctrl-d>")
+      config.unbind("<Ctrl-d>")
+      config.unbind("d")
+      config.unbind("u")
+      config.bind("d", "scroll-page 0 0.5")
+      config.bind("u", "scroll-page 0 -0.5")
+
+      config.unbind("xo")
+      config.unbind("xO")
+      config.bind("x", "tab-close")
+      config.bind("X", "undo")
     '';
   };
 }
