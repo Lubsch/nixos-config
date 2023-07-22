04692646728aedff12680ce4ed69c94736d280d1
Lubsch <33580245+Lubsch@users.noreply.github.com>
2023-07-22 18:32:10 +0200

    cleanup

--- home/common/default.nix
+++ home/common/default.nix
@@ -2,34 +2,30 @@
   imports = map (f: ./. + "/${f}")
     ((builtins.filter (f: f != "default.nix")) (builtins.attrNames (builtins.readDir ./.)));
 
-  options.persist = lib.mkOption { };
-
-  config = {
-    home = {
-      stateVersion = "23.05";
-      packages = with pkgs; [
-        file # File information
-        fq # Explore file formats
-        imagemagick # Convert images
-        entr # Run commands on file change (file watcher)
-        inotify-tools # "
-        poppler_utils # PDF utils (e.g. pdftotext)
-        hyperfine # application benchmarks
-        libqalculate # terminal calculator
-        skim # fuzzy finder
-        zip
-        unzip
-        nix-tree # view a nix derivation's dependencies
-        ncdu # disk usage viewing
-        tokei # count lines of code
-        btop # resource usage viewer
-        neofetch
-        ripgrep # better grep
-        fd # better find
-        magic-wormhole # send files between computers
-      ];
-    };
-
-    systemd.user.startServices = "sd-switch";
+  home = {
+    stateVersion = "23.05";
+    packages = with pkgs; [
+      file # File information
+      fq # Explore file formats
+      imagemagick # Convert images
+      entr # Run commands on file change (file watcher)
+      inotify-tools # "
+      poppler_utils # PDF utils (e.g. pdftotext)
+      hyperfine # application benchmarks
+      libqalculate # terminal calculator
+      skim # fuzzy finder
+      zip
+      unzip
+      nix-tree # view a nix derivation's dependencies
+      ncdu # disk usage viewing
+      tokei # count lines of code
+      btop # resource usage viewer
+      neofetch
+      ripgrep # better grep
+      fd # better find
+      magic-wormhole # send files between computers
+    ];
   };
+
+  systemd.user.startServices = "sd-switch";
 }
--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -2,20 +2,18 @@
   imports = map (f: ./. + "/${f}")
     ((builtins.filter (f: f != "default.nix")) (builtins.attrNames (builtins.readDir ./.)));
 
-  config = {
-    system.stateVersion = "23.05";
-    networking.hostName = hostname;
-    time.timeZone = "Europe/Berlin";
-    environment.enableAllTerminfo = true;
+  system.stateVersion = "23.05";
+  networking.hostName = hostname;
+  time.timeZone = "Europe/Berlin";
+  environment.enableAllTerminfo = true;
 
-    hardware = {
-      enableRedistributableFirmware = true;
-      cpu.${cpuVendor}.updateMicrocode = true;
-    };
-
-    persist.directories = [
-      "/var/lib/systemd/coredump"
-      "/var/log"
-    ];
+  hardware = {
+    enableRedistributableFirmware = true;
+    cpu.${cpuVendor}.updateMicrocode = true;
   };
+
+  persist.directories = [
+    "/var/lib/systemd/coredump"
+    "/var/log"
+  ];
 }
