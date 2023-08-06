fff62e0e0716391b291b8f0a070b35c55e4088f3
Lubsch <33580245+Lubsch@users.noreply.github.com>
2023-08-06 22:15:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 mkdir -p ~/.config/nix
 echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
-nix-shell -p git
-git clone https://github.com/lubsch/nixos-config
-cd nixos-config
-sudo nix run .#disko -- -m disko -f git+file:.#"$1"
-sudo nixos-install --flake .#"$1" --no-root-password
+nix-shell -p git --command ' \
+git clone https://github.com/lubsch/nixos-config \
+&& cd nixos-config \
+&& sudo nix run .#disko -- -m disko -f git+file:.#"$1" \
+&& sudo nixos-install --flake .#"$1" --no-root-password'
