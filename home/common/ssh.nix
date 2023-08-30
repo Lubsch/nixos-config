a63d19f7de150e8a810a150c2558b691e076c0c9
Lubsch <33580245+Lubsch@users.noreply.github.com>
2023-08-30 15:15:35 +0200

    setup git keys

--- home/common/ssh.nix
+++ home/common/ssh.nix
@@ -1,5 +1,17 @@
 {
-  programs.ssh.enable = true;
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      github = {
+        hostname = "github.com";
+        identityFile = "~/.ssh/id_github";
+      };
+      gitlab = {
+        hostname = "git.tu-berlin.de";
+        identityFile = "~/.ssh/id_gitlab";
+      };
+    };
+  };
 
   persist.directories = [ 
     ".ssh"
