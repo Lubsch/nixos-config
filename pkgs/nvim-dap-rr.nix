206fba4b62ae280bce0c2051443473421a17ee9a
Lubsch <33580245+Lubsch@users.noreply.github.com>
2023-10-25 19:21:34 +0200

    update nvim-dap-rr

--- pkgs/nvim-dap-rr.nix
+++ pkgs/nvim-dap-rr.nix
@@ -5,19 +5,19 @@
 
 vimUtils.buildVimPlugin rec {
   pname = "nvim-dap-rr";
-  version = "unstable-2023-09-10";
+  version = "unstable-2023-10-25";
 
   src = fetchFromGitHub {
     owner = "jonboh";
     repo = "nvim-dap-rr";
-    rev = "f1678d5524aac8321c538883e77daa17d6be44f5";
-    hash = "sha256-T8WB7C5JbFbJaQWgrGgqpPHiQBMbGnSPJN/jebuV58M=";
+    rev = "89d41b02f7495e29ea0d4279fb98942b3ccb737d";
+    hash = "sha256-jouY+7rvte90cn8hkFhnB5BJDolkNnuosAvfF0jL0nU=";
   };
 
   meta = with lib; {
     description = "Dap configuration for the record and replay debugger. Supports Rust, C++ and C";
     homepage = "https://github.com/jonboh/nvim-dap-rr";
-    # license = licenses.unfree; # FIXME: nix-init did not found a license
+    license = licenses.mit;
     maintainers = with maintainers; [ ];
     mainProgram = "nvim-dap-rr";
     platforms = platforms.all;
