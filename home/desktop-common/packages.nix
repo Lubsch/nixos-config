64eb5e15f4729b5d1ed0eef7e1583454e3c49a9a
Lubsch <33580245+Lubsch@users.noreply.github.com>
2024-01-21 18:53:13 +0100

    add loupe

--- home/desktop-common/packages.nix
+++ home/desktop-common/packages.nix
@@ -6,6 +6,7 @@
     wev # show pressed keys
     xorg.xeyes # find xwayland programs
     ffmpeg-full # media tools (full for ffplay)
+    loupe # gnome image viewer
     brightnessctl # change brightness
     wl-clipboard # make wl-copy work
     pcmanfm # file manager
