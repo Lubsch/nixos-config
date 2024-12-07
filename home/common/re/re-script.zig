304da01db01584dda0814cb78e8fa71a8cddf514
Lubsch <33580245+Lubsch@users.noreply.github.com>
2024-12-07 17:23:46 +0100

    ""

--- home/common/re/re-script.zig
+++ home/common/re/re-script.zig
@@ -77,8 +77,12 @@ pub fn main() !void {
         // git commit and push if you are child
         if (try std.posix.fork() == 0) {
             var git_commit = std.process.Child.init(&[_][]const u8{ "git", "commit", "--allow-empty-message", "-m", "\"\"" }, allocator);
+            git_commit.stdout_behavior = .Ignore;
+            git_commit.stderr_behavior = .Ignore;
             _ = try git_commit.spawnAndWait();
             var git_push = std.process.Child.init(&[_][]const u8{ "git", "push" }, allocator);
+            git_push.stdout_behavior = .Ignore;
+            git_push.stderr_behavior = .Ignore;
             _ = try git_push.spawnAndWait(); // there's nothing wrong with waiting so we don't orphan a process
         }
     } else {
